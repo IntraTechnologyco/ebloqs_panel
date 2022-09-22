0578e6344e840a44d568d1059d241dc59c9d7fe4
Breiner Lopez <lopezcabezz@gmail.com>
2022-09-22 13:02:33 -0500

    asyncronus script fixed

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head />
-      <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+      <Script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></Script>
       <body>
         <Main />
         <NextScript />
